afbc0b90899db5cbb73bd441caeccf39c855ba52
Anghelo Alva <ance.anghelo@gmail.com>
2024-01-31 21:28:58 -0300

    Add PetsSection component and PetCard component

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,6 +1,9 @@
+import PetsSection from '../components/home/page/Pets-section'
+
 export default function Home (): React.ReactElement {
   return (
-    <main className="flex bg-bg-100 min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <PetsSection />
     </main>
   )
 }
---
+++ app/components/home/page/Pets-section.tsx
@@ -0,0 +1,33 @@
+import PetCard from '../ui/Pet-card'
+
+const pets = [
+  { src: '/images/for-dogs.png', alt: 'For Dogs', href: '/pets/dogs' },
+  { src: '/images/for-cats.png', alt: 'For Cats', href: '/pets/cats' },
+  { src: '/images/for-birds.png', alt: 'For Birds', href: '/pets/birds' },
+  { src: '/images/for-reptiles.png', alt: 'For Reptiles', href: '/pets/reptiles' },
+  { src: '/images/for-small-pet.png', alt: 'For Small Pet', href: '/pets/small-pet' },
+  { src: '/images/for-fish.png', alt: 'For Fish', href: '/pets/fish' }
+]
+
+function PetsSection (): React.ReactElement {
+  return (
+    <section className='px-5 sm:px-10 md:px-20 lg:px-40'>
+      <h2 className='text-3xl font-bold mb-2'>Pets</h2>
+
+      <div className='grid grid-cols-2 gap-2 md:grid-cols-3'>
+        {
+          pets.map((pet, index) => (
+            <PetCard
+              key={index}
+              src={pet.src}
+              alt={pet.alt}
+              href={pet.href}
+            />
+          ))
+        }
+      </div>
+    </section>
+  )
+}
+
+export default PetsSection
