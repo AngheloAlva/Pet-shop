e40db645f97fc11c04abcff2a08ac1e2175bba70
Anghelo Alva <ance.anghelo@gmail.com>
2024-02-10 20:40:58 -0300

    Add authorization check to AdminLayout component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,9 +1,24 @@
+import { auth } from '@clerk/nextjs/server'
 import AdminNavbar from '../components/admin/navbar/Navbar'
 import VerticalAdminNavbar from '../components/admin/navbar/Vertical-navbar'
+import { getUserById } from '../lib/api/user/user'
 
-function AdminLayout (
+async function AdminLayout (
   { children }: Readonly<{ children: React.ReactNode }>
-): React.ReactElement {
+): Promise<React.ReactElement> {
+  const { userId } = auth()
+
+  if (userId === null) return (<></>)
+  const user = await getUserById(userId)
+
+  if (user.role !== 'ADMIN') {
+    return (
+      <main className='flex flex-col gap-7 pt-4 md pb-20 px-5 text-text-100'>
+        <h1 className='text-2xl font-bold'>You are not authorized to view this page</h1>
+      </main>
+    )
+  }
+
   return (
     <>
       <AdminNavbar />
